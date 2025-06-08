c79ce21ba18097b68d80907772ce2d20707c6cde
tomasz97128 <wrx91152@student.wroclaw.merito.pl>
2025-06-08 16:41:50 +0200

    poprawnione wersje plików

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
-import hello_world.views  
+import hello_world.views  # noqa: F401
 
 app = Flask(__name__)
--- hello_world/views.py
+++ hello_world/views.py
@@ -12,11 +12,9 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie, output.lower())
 
 
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
-    
